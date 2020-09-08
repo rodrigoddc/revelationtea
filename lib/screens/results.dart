62cd1fd7971d285d23644b171922b50abb711342
rodrigo <rodrigoddc1@gmail.com>
2020-09-08 00:55:09 -0300

    (done) get only username from user_email to show on results

--- lib/screens/results.dart
+++ lib/screens/results.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:revelationtea/modules/get_all_results.dart';
